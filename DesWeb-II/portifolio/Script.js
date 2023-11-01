0c83703fdeb35df586c16db106541bbd67e663ae
cc23579 <cc23579@g.unicamp.br>
2023-11-01 11:09:52 -0300

    site (quase) pronto

--- DesWeb-II/portifolio/Script.js
+++ DesWeb-II/portifolio/Script.js
@@ -1,4 +1,4 @@
-function adicionarCard(){
+function adicionarCards(){
    
     var filmes = [
         {
@@ -33,59 +33,12 @@ function adicionarCard(){
     
     for (var i = 0; i < filmes.length; i++) {
         
-        var card = document.createElement("div");
-        card.className = "card";
-
-       
-        var titulo = document.createElement("h4");
-        titulo.textContent = filmes[i].titulo;
-        card.appendChild(titulo);
-
-        
-        var imagem = document.createElement("img");
-        imagem.src = "#"; 
-        card.appendChild(imagem);
-
-        
-        var sinopse = document.createElement("p");
-        sinopse.textContent = "Sinopse:";
-        card.appendChild(sinopse);
-
-        var sinopseLabel = document.createElement("label");
-        sinopseLabel.textContent = "Descrição do filme aqui."; 
-        card.appendChild(sinopseLabel);
-
-        
-        var diretor = document.createElement("p");
-        diretor.textContent = "Diretor:";
-        card.appendChild(diretor);
-
-        var diretorLabel = document.createElement("label");
-        diretorLabel.textContent = filmes[i].diretor;
-        card.appendChild(diretorLabel);
-
-        
-        var ano = document.createElement("p");
-        ano.textContent = "Ano de Lançamento:";
-        card.appendChild(ano);
-
-        var anoLabel = document.createElement("label");
-        anoLabel.textContent = filmes[i].ano;
-        card.appendChild(anoLabel);
-
-       
-        var nota = document.createElement("p");
-        nota.textContent = "Nota:";
-        card.appendChild(nota);
-
-        var notaLabel = document.createElement("label");
-        notaLabel.textContent = filmes[i].nota;
-        card.appendChild(notaLabel);
-
-        
-        container.appendChild(card);
-    }
+        document.inner
 
 }
+}
+adicionarCards()
 
-adicionarCard()
+function alerta(){
+    alert("Fazer a função de descer a página no ponto onde está a lista")
+}
